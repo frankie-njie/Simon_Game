cf1397598c0d3c7652c1b5dd2ce8e05de17cf3d8
frankie-njie <njie_frankie@yahoo.com>
2020-10-30 19:38:20 +0100

    some changes

--- game.js
+++ game.js
@@ -1,14 +1,18 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour = buttonColours[nextSequence()];
-let gamePattern = [];
-gamePattern.push(randomChosenColour);
 
+let gamePattern = [];
 
 function nextSequence(){
-  let randomNumber = Math.floor(Math.random() * 3);
-  return randomNumber;
-}
 
-$('#'+randomChosenColour).fadeOut(200).fadeIn(200);
-let audio = new Audio ("sounds/" +randomChosenColour + ".mp3");
-// $('audio').play();
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //return randomNumber;
+
+  // Selecting the buttons with the same Id and playing sound when the animation is done
+ $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  let audio = new Audio ("sounds/" +randomChosenColour + ".mp3");
+  // audio.muted = true;
+  audio.play();
+}

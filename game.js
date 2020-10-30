81b159f3c1d718b46f1a155d35169f96dd08b2e4
frankie-njie <njie_frankie@yahoo.com>
2020-10-30 23:26:04 +0100

    made some changes

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 let userClickedPattern = [];
-
+//nextSequence();
 //adding a click event to the btn class
 $('.btn').click(function(){
 
@@ -11,6 +11,7 @@ $('.btn').click(function(){
   console.log(userClickedPattern);
 
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 
 
@@ -19,18 +20,21 @@ function nextSequence(){
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  // Selecting the buttons with the same Id and playing sound when the animation is done
+  // Selecting the buttons with the same Id
  $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
  playSound(randomChosenColour)
 }
 
 function animatePress(currentColour){
+  $('#' + currentColour).addClass("pressed");
 
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
 
 function playSound(name) {
-  // Selecting the buttons with the same Id and playing sound when the animation is done
   let audio = new Audio ("sounds/" + name + ".mp3");
   audio.play();
 }
